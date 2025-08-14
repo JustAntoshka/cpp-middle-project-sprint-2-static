a195b8c282bd4b674126d92908ae6bcc13b57a98
anton <you@example.com>
2025-08-14 22:09:15 +0000

    implement point 2 of plan

--- include/types.hpp
+++ include/types.hpp
@@ -1,12 +1,37 @@
 #pragma once
 
+#include <stddef.h>
+
 namespace stdx::details {
 
 // Шаблонный класс, хранящий C-style строку фиксированной длины
 
-// ваш код здесь
+template <size_t Len = 0>
 struct fixed_string {
-    // ваш код здесь
+public:
+    constexpr fixed_string(const char (&str)[Len + 1]) {
+        size_t i = 0;
+        for(; i < Len; ++i) {
+            _string[i] = str[i];
+        }
+        _string[i] = '\0';
+    }
+    
+    template <size_t Len2>
+    constexpr fixed_string(const char (&str)[Len2]) {
+        static_assert(Len2 < Len, "Given string is too long");
+        size_t i = 0;
+        for(; i < Len2; ++i) {
+            _string[i] = str[i];
+        }
+        _string[i] = '\0';
+    }
+
+    template <typename It>
+    constexpr fixed_string(It begin, It end);
+
+private:
+    char _string[Len + 1] = {};
 };
 
 // Шаблонный класс, хранящий fixed_string достаточной длины для хранения ошибки парсинга
