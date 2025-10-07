9c23ea87d5674034a72eebd39ee0a8da28bdbe08
anton <anton.tyugaevich@gmail.com>
2025-10-07 23:58:15 +0000

    fix code review comments: use std::from_chars()

--- include/parse.hpp
+++ include/parse.hpp
@@ -5,110 +5,32 @@
 #include <cstring>
 #include <type_traits>
 #include <concepts>
+#include <charconv>
 
 #include "format_string.hpp"
 #include "types.hpp"
 
 
 namespace utils {
-    template<typename T>
-    constexpr const char* MAX_NUM_AS_STR;
-
-    template<typename T>
-    constexpr const char* MIN_NUM_AS_STR;
-
-    template<> constexpr const char MIN_NUM_AS_STR<int8_t>[] = "-128";
-    template<> constexpr const char MAX_NUM_AS_STR<int8_t>[] = "127";
-
-    template<> constexpr const char MIN_NUM_AS_STR<uint8_t>[] = "0";
-    template<> constexpr const char MAX_NUM_AS_STR<uint8_t>[] = "255";
-
-    template<> constexpr const char MIN_NUM_AS_STR<int16_t>[] = "-32768";
-    template<> constexpr const char MAX_NUM_AS_STR<int16_t>[] = "32767";
-
-    template<> constexpr const char MIN_NUM_AS_STR<uint16_t>[] = "0";
-    template<> constexpr const char MAX_NUM_AS_STR<uint16_t>[] = "65535";
-
-    template<> constexpr const char MIN_NUM_AS_STR<int32_t>[] = "-2147483648";
-    template<> constexpr const char MAX_NUM_AS_STR<int32_t>[] = "2147483647";
-
-    template<> constexpr const char MIN_NUM_AS_STR<uint32_t>[] = "0";
-    template<> constexpr const char MAX_NUM_AS_STR<uint32_t>[] = "4294967295";
-
-    template<> constexpr const char MIN_NUM_AS_STR<int64_t>[] = "-9223372036854775808";
-    template<> constexpr const char MAX_NUM_AS_STR<int64_t>[] = "9223372036854775807";
-
-    template<> constexpr const char MIN_NUM_AS_STR<uint64_t>[] = "0";
-    template<> constexpr const char MAX_NUM_AS_STR<uint64_t>[] = "18446744073709551615";
-
-    consteval const char* trim_zeros(const char *str) {
-        while(*str == '0') str++;
-        return *str ? str : str - 1;
-    }
-
-    consteval bool is_digit(char c) {
-        return '0' <= c && c <= '9';
-    }
-
-    consteval bool is_negative(const char* str) {
-        return str[0] == '-';
-    }
-
-    consteval bool is_positive_number(const char *str) {
-        if(!*str) return false;
-        while(*str && is_digit(*str)) ++str;
-        return !*str;
-    }
-
-    consteval bool is_number(const char *str) {
-        return is_negative(str) ? is_positive_number(str + 1) : is_positive_number(str);
-    }
-
-    consteval bool greater_or_eq_abs(const char *str1, const char *str2) {
-        auto trimmed_str1 = trim_zeros(str1);
-        auto trimmed_str2 = trim_zeros(str2);
-
-        const size_t LEN_1 = std::strlen(trimmed_str1);
-        const size_t LEN_2 = std::strlen(trimmed_str2);
-
-        if(LEN_1 != LEN_2) {
-            return LEN_1 > LEN_2;
-        }
-
-        while(*trimmed_str1 && *trimmed_str1 == *trimmed_str2) {
-            ++trimmed_str1;
-            ++trimmed_str2;
-        }
-        return *trimmed_str1 >= *trimmed_str2;
-    }
-
-    template<size_t N1, size_t N2>
-    consteval bool greater_or_eq(const char (&str1)[N1], const char (&str2)[N2]) {
-        if (is_negative(str1) && is_negative(str2)) {
-            return greater_or_eq_abs(str2 + 1, str1 + 1);
-        }
-        if (!is_negative(str1) && !is_negative(str2)) {
-            return greater_or_eq_abs(str1, str2);
-        }
-        return is_negative(str2);
-    }
-
-    template<typename T, size_t N>
-    consteval bool is_in_range(const char (&str)[N]) {
-        return greater_or_eq(str, MIN_NUM_AS_STR<std::remove_cv_t<T>>) && greater_or_eq(MAX_NUM_AS_STR<std::remove_cv_t<T>>, str);
+    template <typename T>
+    consteval std::pair<std::from_chars_result, T> parse_number_impl(const char* str) {
+        std::remove_cv_t<T> result{};
+        auto len = std::strlen(str);
+        auto parse_result = std::from_chars(str, str + len, result);
+        return {parse_result, result};
     }
 
     template <typename T, stdx::details::fixed_string src>
     consteval T parse_number() {
-        constexpr bool is_neg = is_negative(src.data);
-        const char* str = is_neg ? src.data + 1 : src.data;
-        std::remove_cv_t<T> result = is_neg ? ('0' - *str) : (*str - '0');
-        while(*++str) {
-            result *= 10;
-            result += is_neg ? ('0' - *str) : (*str - '0');
-        }
-        return result;
+        constexpr auto parse_result = parse_number_impl<T>(src.data);
+
+        static_assert((parse_result.first.ec != std::errc::invalid_argument) && (parse_result.first.ptr == src.data + std::strlen(src.data)), "Source string is not a valid number");
+        static_assert(parse_result.first.ec != std::errc::result_out_of_range, "Number is out of given type range");
+        static_assert(parse_result.first.ec == std::errc(), "Error occured while parsing source string");
+
+        return parse_result.second;
     }
+    
 } // namespace utils
 
 namespace stdx::details {
@@ -175,7 +97,6 @@ consteval T parse_value_impl() {
 
 template<std::unsigned_integral T, fixed_string src>
 consteval T parse_value_impl() {
-    static_assert(!utils::is_negative(src.data), "Unable to parse a negative number string into an unsigned type");
     return utils::parse_number<T, src>();
 }
 
@@ -198,16 +119,12 @@ constexpr bool operator==(const fixed_string<Size>& lhs, const fixed_string<Size
 template <typename T, fixed_string spec, fixed_string src>
 requires (spec == fixed_string("%d"))
 consteval T parse_value() {
-    static_assert(utils::is_number(src.data), "Source string is not a valid number");
-    static_assert(utils::is_in_range<T>(src.data), "Number is out of given type range");
     return parse_value_impl<T, src>();
 }
 
 template <typename T, fixed_string spec, fixed_string src>
 requires (spec == fixed_string("%u"))
 consteval T parse_value() {
-    static_assert(utils::is_positive_number(src.data), "Source string is not a valid number");
-    static_assert(utils::is_in_range<T>(src.data), "Number is out of given type range");
     static_assert(std::is_unsigned_v<T>, "Incorrect type provided");
     return parse_value_impl<T, src>();
 }
